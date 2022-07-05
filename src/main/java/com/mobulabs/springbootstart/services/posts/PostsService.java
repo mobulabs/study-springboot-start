db3f68a0c98b11d62046235e1e7ca4c750220965
mobulabs <mobulabs@gmail.com>
2022-07-05 22:13:40 +0900

    Add save function

---
+++ src/main/java/com/mobulabs/springbootstart/services/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.mobulabs.springbootstart.services.posts;
+
+import com.mobulabs.springbootstart.domain.posts.PostsRepository;
+import com.mobulabs.springbootstart.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/mobulabs/springbootstart/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.mobulabs.springbootstart.web;
+
+import com.mobulabs.springbootstart.services.posts.PostsService;
+import com.mobulabs.springbootstart.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
