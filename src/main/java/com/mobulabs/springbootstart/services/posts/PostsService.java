70f3606494a594dd459bc14fb5df898daa021a01
mobulabs <mobulabs@gmail.com>
2022-07-06 22:58:18 +0900

    Add update, view function

--- src/main/java/com/mobulabs/springbootstart/services/posts/PostsService.java
+++ src/main/java/com/mobulabs/springbootstart/services/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.mobulabs.springbootstart.services.posts;
 
+import com.mobulabs.springbootstart.domain.posts.Posts;
 import com.mobulabs.springbootstart.domain.posts.PostsRepository;
+import com.mobulabs.springbootstart.web.dto.PostResponseDto;
 import com.mobulabs.springbootstart.web.dto.PostsSaveRequestDto;
+import com.mobulabs.springbootstart.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostResponseDto(entity);
     }
 }
