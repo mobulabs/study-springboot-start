8aa88d094aa0513ff941dff3c9276e513592c1db
mobulabs <mobulabs@gmail.com>
2022-07-11 15:42:03 +0900

    Add BaseTimeEntity

--- src/test/java/com/mobulabs/springbootstart/web/HelloControllerTest.java
+++ src/test/java/com/mobulabs/springbootstart/web/HelloControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
