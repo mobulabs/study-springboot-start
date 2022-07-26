7b29027761facb34a82c7077b87fa8f8445b2466
mobulabs <mobulabs@gmail.com>
2022-07-26 17:28:39 +0900

    Configure auto deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=study-springboot-start
+
+echo "> Copy build file"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> Check pid of application running"
+
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep java | awk '{print $1}')
+
+echo "> PID of application currently running: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+        echo "> No application are currently running"
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> Deploy new application"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> Running $JAR_NAME"
+
+chmod +x $JAR_NAME
+
+nohup java -jar \
+        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.profiles.active=real \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
